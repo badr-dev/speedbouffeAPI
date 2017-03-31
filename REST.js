6214e42a37abc92f8dfe0a27310f687d63a4a4ce
badr-dev <badredine.belhadef@epitech.eu>
2017-03-31 17:35:01 +0200

    Avancement Import

--- REST.js
+++ REST.js
@@ -37,15 +37,23 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         });
     });
 
-    router.post("/import", function(req, res) {
-        res.json({
-            "Acheteur" : req.body.Acheteur,
-            "Infos_commande" : req.body.Film,
-            "Details_commande" : req.body.Details_commande
+    router.get("/acheteur", function(req, res){
+        var query = "SELECT * FROM ??";
+        var table = ["acheteur"];
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
         });
+    });
 
-        var query = "INSERT INTO ??(??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?)";
-        var table = [
+    router.post("/import", function(req, res) {
+        // Create user
+        var q_user = "INSERT INTO ??(??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?)";
+        var t_user = [
             "acheteur",
             "nom", 
             "prenom", 
@@ -60,6 +68,44 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
             2,
             1
         ];
+        
+        q_user = mysql.format(q_user, t_user);
+        connection.query(q_user, function(err, rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                var id_user = 1;
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+  
+        // Create commands
+
+        var q_order = "INSERT INTO ??(??, ??, ??, ??) VALUES (?, ?, ?, ?)";
+        var t_order = [
+            "commandes",
+            "jour_commande", 
+            "heure_livraison", 
+            "type_payement_id", 
+            "acheteur_id", 
+            req.body.Film.Jour,
+            req.body.Film.Horaire_livraison,
+            1,
+            id_user
+        ];
+        
+        q_order = mysql.format(q_order, t_order);
+        connection.query(q_order, function(err, rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Order Added !"});
+            }
+        });
+
+        // Create commands details
+        
+        // TODO
     });
 
     router.post("/users",function(req,res){
